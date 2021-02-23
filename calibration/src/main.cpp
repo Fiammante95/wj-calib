ba9c5f83bf6d42398d44a2bc605a58f31f9c2cb4
Fiammante95 <43271881+Fiammante95@users.noreply.github.com>
2021-02-23 11:30:34 +0100

    restored original git program

--- calibration/src/main.cpp
+++ calibration/src/main.cpp
@@ -1,5 +1,61 @@
 #include <Arduino.h>
 
+/*==========================================
+ * Arduino Labview Voltage Display
+ * Created by zxlee (iamzxlee.wordpress.com)
+ *
+ * Demonstrate how to display Arduino analog
+ * inputs, A0 and A1 to Labview using Serial
+ * communication
+============================================*/
+
+#define input0 A0
+#define input1 A1
+
+unsigned long previousCount = 0;
+
+/**==========================================
+ * Constants for interval between samples
+ * 50000 microseconds = 50 milliseconds
+ * equivalent to 20 samples per second
+ *
+ * Sample rate is controlled using the
+ * method similar to example BlinkWithoutDelay
+============================================*/
+
+const unsigned long interval = 20000;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop()
+{
+  unsigned long currentCount = micros();
+
+  //Calculates the time difference since last serial data sent
+  unsigned long countDiff = currentCount - previousCount;
+
+  if(countDiff >= interval)
+  {
+    //Create a buffer to store string
+    char buffer[50];
+
+    //Format the data into string
+    sprintf(buffer, "$%d,%d,%u\r\n", analogRead(input0), analogRead(input1), countDiff);
+
+    //Write string into COM port
+    Serial.write(buffer);
+
+    //Update new previous count
+    previousCount = currentCount;
+  }
+
+}
+
+
 // //Constants:
 //
 // const int fsamp = 10; //Hz
@@ -46,21 +102,23 @@
  * An IR detector/demodulator must be connected to the input RECV_PIN.
  * Initially coded 2009 Ken Shirriff http://www.righto.com/
  */
-
- int sensorPin = A0;   // select the analog input pin for the photoresistor
- int laserPin = 10;
-
-
- void setup() {
-   Serial.begin(9600);
-   analogWrite(laserPin,200);
- }
-
-
- void loop() {
-   Serial.println(analogRead(sensorPin));
-   delay(100);
- }
+ //
+ // int sensorPin = A0;   // select the analog input pin for the photoresistor
+ // int laserPin = 10;
+ //
+ //
+ // void setup() {
+ //   Serial.begin(9600);
+ //   //analogWrite(laserPin,200);
+ // }
+ //
+ //
+ // void loop() {
+ //   Serial.print(analogRead(sensorPin));
+ //
+ //   Serial.println(analogRead(A1));
+ //   delay(100);
+ // }
 
 // **************************************************************************
 
